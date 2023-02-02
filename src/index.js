d5aa49cec2a985b277bda0bf40b494e4aa6ce52a
luizzakseski <l.zakseski@gmail.com>
2023-02-02 18:32:02 -0300

    removed exports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+
 Amplify.configure(awsExports);
 
 
